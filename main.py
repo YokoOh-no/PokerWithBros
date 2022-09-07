fb0f352804f771fdae98dd6e2176ba3ccd36d9ce
dom-lafluer <domparish@mailfence.com>
2022-09-06 23:32:47 -0500

    - build deck function works
    random module imported to implement a schuffle method

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
+# Welcome to Poker witht the bros dource code.
+import random
 class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank 
     def show(self):
         print('{} of {}'.format(self.rank, self.suit)) # method that is used on a single card to identify
-# card = Card('card', 5)
+# card = Card('card', 5) ---> card instanciation test
 # card.show()
 
 
 class Deck:
-    def __init__(self) -> None:
+    def __init__(self):
         self.cards = []
         self.build()
     def build(self):
-        for s in ['Spades', 'Hearts', 'Clubs', 'Diamonds']:
-            for r in range(1,14):
-                self.cards.append(Card(s, r))
+        for s in ['Spades', 'Hearts', 'Clubs', 'Diamonds']: # loop to assign suits to cards
+            for r in range(1,14): # nested loop assigning rank to cards
+                self.cards.append(Card(s, r)) # card class objects imstanciated insife of deck class 
     def show(self):
         for c in self.cards:
-            print(c)
+            c.show()
+# test_deck = Deck()
+# test_deck.show()
 class Player:
     pass
